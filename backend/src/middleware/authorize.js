4aeaeac57ddaee25aef3487f0425b687e0255207
sumanth0099 <tetalasumanthreddy@gmail.com>
2026-01-03 11:56:31 +0530

    Added authorize middleware

---
+++ backend/src/middleware/authorize.js
@@ -0,0 +1,9 @@
+const authorize = (allowedRoles) => (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ success: false, message: 'Forbidden: insufficient permissions' });
+    }
+    next();
+  };
+  
+module.exports = authorize;
+  
